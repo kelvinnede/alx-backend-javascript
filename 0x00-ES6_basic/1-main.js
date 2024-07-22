6004f6f03d1b324488571c938661ba85a71fdc84
kelvinnede <kelvinnede@gmail.com>
2024-07-22 04:53:22 -0700

    1-main.js

--- 0x00-ES6_basic/1-main.js
+++ 0x00-ES6_basic/1-main.js
@@ -1,5 +1,4 @@
-// 1-main.js
 import taskBlock from './1-block-scoped.js';
 
-console.log(taskBlock(true));  // [ false, true ]
-console.log(taskBlock(false)); // [ false, true ]
+console.log(taskBlock(true));
+console.log(taskBlock(false));
