1040fa416f8363262231331208caec5d9afe94f8
kelvinnede <kelvinnede@gmail.com>
2024-07-22 14:55:21 -0700

    10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let value of array) {
-    const index = array.indexOf(value);
-    array[index] = appendString + value;
+  let idx = 0;
+  for (const value of array) {
+    /* eslint-disable no-param-reassign */
+    array[idx] = appendString + value;
+    idx += 1;
   }
 
   return array;
