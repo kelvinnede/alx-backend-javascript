91bef1aca902b3740ec9be3b37c21d170ca27851
kelvinnede <kelvinnede@gmail.com>
2024-07-21 23:40:00 -0700

    0-main.js

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,17 @@
+// 0-constants.js
+
+export function taskFirst() {
+  const task = 'I prefer const when I can.'; // Use const for task
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let'; // Use let for combination
+  combination += getLast();
+
+  return combination;
+}
