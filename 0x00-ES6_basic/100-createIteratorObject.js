de4192f61c64e7f46c0c2575bc3098da2765dede
kelvinnede <kelvinnede@gmail.com>
2024-07-22 05:57:26 -0700

    100-main.js

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,11 @@
+export default function createIteratorObject(report) {
+  function* employeeGenerator() {
+    for (const department of Object.values(report.allEmployees)) {
+      for (const employee of department) {
+        yield employee;
+      }
+    }
+  }
+
+  return employeeGenerator();
+}
