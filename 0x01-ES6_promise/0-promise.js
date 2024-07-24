f63754e564428d276d0b10819c0d81c2b86a1a9b
kelvinnede <kelvinnede@gmail.com>
2024-07-24 01:00:53 -0700

    new

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,8 @@
+// 0-promise.js
+
 export default function getResponseFromAPI() {
-    return new Promise((resolve) => {
-        resolve('Success');
-    });
+  return new Promise((resolve, reject) => {
+    // You can resolve or reject the promise here
+    resolve('Success');
+  });
 }
