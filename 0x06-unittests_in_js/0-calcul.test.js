d648f73f32319b3969ae33be3d511fefd1a471eb
kelvinnede <kelvinnede@gmail.com>
2024-09-20 01:41:42 -0700

    last

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,26 +1,31 @@
-// 0-calcul.test.js
+const assert = require("assert");
+const { it, describe } = require("mocha");
+const calculateNumber = require("./0-calcul");
 
-const assert = require('assert');
-const calculateNumber = require('./0-calcul');
+describe("calculateNumber()", function() {
 
-describe('calculateNumber', () => {
-    it('should return 4 when adding 1 and 3', () => {
-        assert.strictEqual(calculateNumber(1, 3), 4);
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(1, 2);
+      assert.strictEqual(res, 3);
     });
-
-    it('should return 5 when adding 1 and 3.7', () => {
-        assert.strictEqual(calculateNumber(1, 3.7), 5);
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(1.4, 2.2);
+      assert.strictEqual(res, 3);
     });
-
-    it('should return 5 when adding 1.2 and 3.7', () => {
-        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(1.6, 2.7);
+      assert.strictEqual(res, 5);
     });
-
-    it('should return 6 when adding 1.5 and 3.7', () => {
-        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(0, 0);
+      assert.strictEqual(res, 0);
     });
-
-    it('should return 0 when adding 0 and 0', () => {
-        assert.strictEqual(calculateNumber(0, 0), 0);
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(-1.6, -1.7);
+      assert.strictEqual(res, -4);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(-1.4, -1.3);
+	assert.strictEqual(res, -2);
     });
 });
